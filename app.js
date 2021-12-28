e141aa6b9c6ddb1ac19c2ab20ec245b5bf19adb5
Jitin <jitin.a27.jd@gmail.com>
2021-12-28 12:29:19 +0530

    commit stocks added instead of indexes

--- app.js
+++ app.js
@@ -1,69 +1,68 @@
+require = require("esm")(module);
 const express=require("express");
 const bodyParser=require("body-parser");
 const _=require("lodash");
 const ejs=require("ejs");
+const stock=require(__dirname+"/stock.js");
 const app=express();
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-var indice_name='';
+var symbol='';
 var open=0;
 var high=0;
 var low=0;
 var current=0;
-
+var transactions=[];
+var quantity=0;
 app.get("/",function(req,res){
-  res.render("index");
+  res.render("aboutus");
 });
-app.post("/",function(req,res){
-  indice_name=req.body.share;
-  var axios = require("axios").default;
-
-var options = {
-  method: 'GET',
-  url: 'https://nse-data1.p.rapidapi.com/equity_stock_indices',
-  params: {symbol: indice_name},
-  headers: {
-    'x-rapidapi-host': 'nse-data1.p.rapidapi.com',
-    'x-rapidapi-key': '2c10a421e6msh912b4fa699d8b38p11b31djsn5560fcad7258'
-  }
-};
-
-axios.request(options).then(function (response) {
-	console.log(response.data);
-   open=response.data.body.metadata.open;
-   high=response.data.body.metadata.high;
-   low=response.data.body.metadata.low;
-   current=response.data.body.metadata.last;
-  res.render("price",{
-    indice_name:indice_name,
-    open:open,
-    high:high,
-    low:low,
-    current:current
-  });
-
-}).catch(function (error) {
-	console.error(error);
-});
+app.post("/index",function(req,res){
+  symbol=req.body.share;
+  let k=stock.getPrice(symbol);
+  k.then(data => { open=data.priceInfo.open;
+    low=data.priceInfo.intraDayHighLow.min;
+    high=data.priceInfo.intraDayHighLow.max;
+    current=data.priceInfo.lastPrice;
+    res.render("price",{
+      symbol:symbol,
+      open:open,
+      high:high,
+      low:low,
+      current:current
+    });});
 });
 app.get("/insights",function(req,res){
   res.render("insights");
 });
 app.get("/transactions",function(req,res){
-  res.render("transactions");
+  res.render("transactions",{transactions:transactions});
 });
-app.get("/aboutus",function(req,res){
-  res.render("aboutus");
+app.get("/index",function(req,res){
+  res.render("index");
 });
 app.get("/buy",function(req,res){
   res.render("buy",{
-    indice_name:indice_name,
+    symbol:symbol,
     open:open,
     high:high,
     low:low,
     current:current
   });
+});
+app.post("/buy",function(req,res){
+  quantity=req.body.quantity;
+  const price_paid=current*quantity;
+  transaction={
+    symbol:symbol,
+    quantity:quantity,
+    current:current,
+    price_paid:price_paid
+  };
+  transactions.push(transaction);
+  console.log(transactions);
+  res.redirect("index");
 });
 app.get("/sell",function(req,res){
   res.render("sell");
